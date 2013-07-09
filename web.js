621446e6e9eb03bd378aad1b75778e7bb30c996c
Ubuntu <ubuntu@ip-172-31-16-139.us-west-2.compute.internal>
2013-07-09 21:03:12 +0000

    Changed web.js to read inde.html

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buf = new Buffer(256);
+
+fs.readFile('./index.html', function (err, data) {
+  if (err) throw err;
+  buf.write(data);
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
